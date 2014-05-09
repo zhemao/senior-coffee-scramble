273b84ef1a036201b74e130fc892f004822ee45b
Howard Mao <zhehao.mao@gmail.com>
2014-05-08 19:56:57 -0700

    make email sending more robust

--- src/senior_coffee_scramble/mailer.clj
+++ src/senior_coffee_scramble/mailer.clj
@@ -14,16 +14,41 @@
    :port (Integer/parseInt (getenv "SMTP_PORT" "25"))
    :tls (= "true" (getenv "SMTP_TLS"))})
 
+(defn create-message [recipient subject text-body html-body]
+  {:to recipient
+   :from EMAIL_FROM
+   :subject subject
+   :body [:alternative
+          {:type "text/plain"
+           :content text-body}
+          {:type "text/html"
+           :content html-body}]})
+
+(def RETRY_DELAY 500)
+(def SENDMSG_RETRIES 2)
+
+(defn attempt-send [message]
+  (try
+    (postal/send-message EMAIL_CONFIG message)
+    (catch Exception e {:code -1, :error :EXCEPTION, :message (str e)})))
+
+(defn retry-function [action check exc-format retries dly]
+  (loop [res (action)
+         retries-left retries]
+    (cond
+      (check res) nil
+      (zero? retries-left) (throw (Exception. (exc-format res)))
+      :else (do
+              (Thread/sleep dly)
+              (recur (action) (dec retries-left))))))
+
 (defn send-email [recipient subject text-body html-body]
-  (postal/send-message EMAIL_CONFIG
-    {:to recipient
-     :from EMAIL_FROM
-     :subject subject
-     :body [:alternative
-            {:type "text/plain"
-             :content text-body}
-            {:type "text/html"
-             :content html-body}]}))
+  (let [message (create-message recipient subject text-body html-body)]
+    (retry-function #(attempt-send message)
+                    (comp zero? :code)
+                    #(str "Failed sending to " recipient ", "
+                          "Error message: " (:message %))
+                    SENDMSG_RETRIES RETRY_DELAY)))
 
 (defn send-confirmation-email [inviter invitations]
   (let [obfuscated-id (obfuscate (:id inviter))
